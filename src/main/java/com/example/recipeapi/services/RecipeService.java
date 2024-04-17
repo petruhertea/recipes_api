9a56f2f91953e43748d421f12431ea435120b2bc
Petru <petre.hertea@gmail.com>
2024-04-17 18:02:15 +0300

    adjusted filtering logic further for available ingredients

--- src/main/java/com/example/recipeapi/services/RecipeService.java
+++ src/main/java/com/example/recipeapi/services/RecipeService.java
@@ -18,29 +18,25 @@ public class RecipeService {
         return recipeRepository.getAllRecipeDetails();
     }
 
-    public List<RecipeDetails> getRecipesFromAvailableIngredients(List<String> availableIngredients) {
-        return recipeRepository.getRecipeDetailsByAvailableIngredients(availableIngredients);
-    }
-
     public List<RecipeDetails> getRecipesFromAvailableIngredients(Map<String, Double> availableIngredients) {
         var temp = recipeRepository.getAllRecipeDetails();
         var set = availableIngredients.keySet();
 
         return temp.stream()
                 .filter(e -> {
-                    String[] ingredients = e.getIngredients()
-                            .split(",");
+                    String[] ingredients = e.getIngredients().split(",");
+                    boolean hasAtLeastOneIngredient = false;
                     for (String ingredient : ingredients) {
                         String[] details = ingredient.split(":");
-                        if (!set.contains(details[0].trim())) {
-                            return false;
-                        }
-                        double weight = extractWeight(details[1].trim());
-                        if (weight > availableIngredients.get(details[0].trim())) {
-                            return false;
+                        if (set.contains(details[0].trim())) {
+                            hasAtLeastOneIngredient = true;
+                            double weight = extractWeight(details[1].trim());
+                            if (weight > availableIngredients.get(details[0].trim())) {
+                                return false; // If the weight is more than available, discard recipe
+                            }
                         }
                     }
-                    return true;
+                    return hasAtLeastOneIngredient; // Return true if at least one ingredient matches
                 })
                 .toList();
     }
